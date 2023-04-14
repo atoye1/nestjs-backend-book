21e39708f07dec58c9ad799393cf39817865a245
SeolDonghun <ssalssi1@gmail.com>
2023-04-14 15:16:36 +0900

    update gitignore

---
+++ user-service/src/migrations/1681441033542-CreateUserTable.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateUserTable1681441033542 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}

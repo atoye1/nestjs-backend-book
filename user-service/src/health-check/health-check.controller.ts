c3e7002d9261151a105ec02ea8cd513c9faefc16
SeolDonghun <ssalssi1@gmail.com>
2023-04-18 11:59:56 +0900

    Add basic health-checker

---
+++ user-service/src/health-check/health-check.controller.ts
@@ -0,0 +1,27 @@
+import { HttpModule } from '@nestjs/axios';
+import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  HttpHealthIndicator,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
+
+@Controller('health-check')
+export class HealthCheckController {
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+    private db: TypeOrmHealthIndicator,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+      () => this.http.pingCheck('localhost', 'http://localhost:3000'),
+      () => this.db.pingCheck('database'),
+    ]);
+  }
+}
